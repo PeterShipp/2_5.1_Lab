2b610958c603015853193830539b687359355cc1
PeterShipp <pshipp81@aol.com>
2020-02-18 13:00:03 -0800

    Add project files.

---
+++ 2_5.1_Lab/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2_5._1_Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello Peter!");
+        }
+    }
+}
